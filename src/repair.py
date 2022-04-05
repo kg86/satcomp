7e71fe02fd4c007efae2f9026e271e2df9206cd1
Dominik Köppl <git@dkppl.de>
2022-04-05 17:55:23 +0900

    add repair as upper bound on grammar size

--- src/repair.py
+++ src/repair.py
@@ -1,16 +1,15 @@
 import argparse
+import os
 import sys
-from typing import List, Tuple
+import json
+import time
 
-
-def mostfreq(inttext: List[int]) -> Tuple[Tuple[int, int], int]:
+def mostfreq(inttext : [int]) -> (int,int):
     frequencies = dict()
     parity = 0
-    for i in range(0, len(inttext) - 1):
-        pair = (inttext[i], inttext[i + 1])
-        if (
-            inttext[i] == inttext[i + 1]
-        ):  # do not count character runs doubly like aaa -> 2
+    for i in range(0, len(inttext)-1):
+        pair = (inttext[i],inttext[i+1])
+        if inttext[i] == inttext[i+1]: #do not count character runs doubly like aaa -> 2
             parity += 1
             if parity == 2:
                 parity = 0
@@ -22,10 +21,9 @@ def mostfreq(inttext: List[int]) -> Tuple[Tuple[int, int], int]:
             frequencies[pair] += 1
         else:
             frequencies[pair] = 1
-    freqlist = list(frequencies.items())
+    freqlist=list(frequencies.items())
     freqlist.sort(key=lambda x: x[1])
     return freqlist[-1]
-
 
 def repair(text: bytes) -> int:
     print(text)
@@ -41,22 +39,18 @@ def repair(text: bytes) -> int:
         # print(inttext)
         chosen_entry = mostfreq(inttext)
         # print(chosen_entry)
-        if chosen_entry[1] == 1:
+        if chosen_entry[1] == 1: 
             break
         chosen_pair = chosen_entry[0]
-        for i in range(0, len(inttext) - 1):
-            if i >= len(inttext) - 1:
+        for i in range(0, len(inttext)-1):
+            if i >= len(inttext)-1:
                 break
-            charA = inttext[i]
-            charB = inttext[i + 1]
-            pairA = chosen_pair[0]
-            pairB = chosen_pair[1]
-            if charA == pairA and charB == pairB:
+            if inttext[i] == chosen_pair[0] and inttext[i+1] == chosen_pair[1]:
                 inttext[i] = last_nonterminal
-                inttext.pop(i + 1)
+                inttext.pop(i+1)
         last_nonterminal += 1
     # print(len(inttext) + last_nonterminal - FIRST_NONTERMINAL)
-    return 1 + len(inttext) + last_nonterminal - FIRST_NONTERMINAL
+    return len(inttext) + last_nonterminal - FIRST_NONTERMINAL
 
 
 def parse_args():
@@ -64,7 +58,7 @@ def parse_args():
     parser.add_argument("--file", type=str, help="input file", default="")
     parser.add_argument("--str", type=str, help="input string", default="")
     args = parser.parse_args()
-    if args.file == "" and args.str == "":
+    if ( args.file == "" and args.str == ""):
         parser.print_help()
         sys.exit()
     return args
@@ -77,4 +71,4 @@ if __name__ == "__main__":
         text = args.str
     else:
         text = open(args.file, "rb").read()
-    print(repair(text))
+    repair(text)
