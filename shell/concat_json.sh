061dd43356dbfa29f45d8d7c1acc66b68e32e6e5
Dominik Köppl <git@dkppl.de>
2022-04-14 14:30:23 +0900

    add shell scripts

--- shell/concat_json.sh
+++ shell/concat_json.sh
@@ -1,42 +1,19 @@
-#!/usr/bin/env bash
-
-
-function die {
-	echo "$1" >&2
-	exit 1
-}
-
-[[ "$#" -eq 1 ]] || die "Usage: $0 [log-folder]"
-logFolder=$(readlink -e "$1")
-[[ -d "$logFolder" ]] || die "$logFolder is not a directory"
-
+#!/bin/zsh
 echo '['
 ((counter=0))
-for filename in $logFolder/*.log; do
-	filebasename=$(basename $filename)
-	algo=$(echo $filebasename | cut -f1 -d'_')
-	dataset=$(echo $filebasename | cut -f2- -d'_')
-	datasetbasename="$(basename $dataset .log)"
-	if echo "$datasetbasename" | grep -q '\.[0-9][0-9][0-9][0-9]$'; then
-		file_len=$(echo "$datasetbasename" | sed 's@.*\.\([0-9][0-9][0-9][0-9]\)$@\1@')
-		datasetbasename=$(echo "$datasetbasename" | sed 's@\.[0-9][0-9][0-9][0-9]$@@')
-	fi
-	# echo $dataset
-	# return
-
-	if ! grep -q '^{' "$filename"; then
-		[[ "$algo" = "attr" ]] && continue
-		errfile=$filename:r.err
-		Status='unknown'
-		grep -q "DUE TO TIME LIMIT" "$errfile" && Status="no time"
-		grep -q "cgroup out-of-memory" "$errfile" && Status="no mem"
-		[[ "$counter" -gt 0 ]] && echo ', '
-		((counter++))
-		echo "{\"algo\": \"$algo\", \"status\": \"$Status\", \"file_name\": \"$dataset\", \"dataset\": \"$datasetbasename\", \"file_len\": \"$file_len\" }"
-		continue
-	fi
+for filename in *.log; do
 	[[ "$counter" -gt 0 ]] && echo ', '
 	((counter++))
-	grep '^{' "$filename" | sed "s@\"algo\": \"[^\"]\+\"@\"algo\": \"$algo\",\"dataset\": \"$datasetbasename\"@"
+	algo=$(echo $filename | cut -f1 -d'_')
+	dataset=$(echo $filename | cut -f2- -d'_')
+	if ! grep -q '^{' "$filename"; then
+		errfile=$filename:r.err
+		grep -q "DUE TO TIME LIMIT" "$errfile" && Status="no time"
+		grep -q "cgroup out-of-memory" "$errfile" && Status="no mem"
+		echo "{\"algo\": \"$algo\", \"status\": \"$Status\", \"file_name\": \"$dataset\" }"
+		continue
+	fi
+	grep '^{' "$filename" | sed "s@\"solver\"@\"$algo\"@"
 done
 echo ']'
+
--- shell/generate_slurmscripts.sh
+++ shell/generate_slurmscripts.sh
@@ -1,30 +1,6 @@
-#!/usr/bin/env bash
-
-kMaxMemory='16000'
-
-function die {
-	echo "$1" >&2
-	exit 1
-}
-
-[[ "$#" -eq 3 ]] ||
-	die "Usage: $0 [dataset-folder] [log-folder] [script-folder]
-	- dataset-folder: folder in which the datasets are stored
-	- log-folder: folder in which the log files to write
-	- script-folder: folder in which the executable scripts to write"
-
-[[ -d "$1" ]] || die "$1 is not a directory"
-datasetFolder=$(readlink -e "$1")
-[[ -d "$datasetFolder" ]] || die "$datasetFolder is not a directory"
-
-mkdir -p "$2" || die "cannot create folder $2"
-logFolder=$(readlink -e "$2")
-[[ -d "$logFolder" ]] || die "$logFolder is not a directory"
-
-mkdir -p "$3" || die "cannot create folder $3"
-scriptFolder=$(readlink -e "$3")
-[[ -d "$scriptFolder" ]] || die "$scriptFolder is not a directory"
-
+#!/bin/bash
+set -e
+set -x
 
 scriptpath=`dirname $(readlink -f "$0")`
 
@@ -32,50 +8,36 @@ function putScript {
 command="$1"
 basename="$2"
 filename="$3"
-memory="$4"
 cat <<EOF
 #!/bin/zsh
 #
 #SBATCH --job-name=${basename} # Job name
 #SBATCH --ntasks=9                    # Run on a single CPU
-#SBATCH --mem=${memory}                     # memory in megabyte
+#SBATCH --mem=16000                     # memory in megabyte
 #SBATCH --time=01:00:00               # Time limit hrs:min:sec
-#SBATCH --output=$logFolder/${basename}.log   # stdout
-#SBATCH --error=$logFolder/${basename}.err    # stderr
-#
-#PBS -N ${basename}
-#PBS -l walltime=01:00:00,mem=${memory}mb,nodes=1,procs=1
-#PBS -o $logFolder/${basename}.log
-#PBS -e $logFolder/${basename}.err
-
-
+#SBATCH --output=${HOME}/log/${basename}.log   # stdout
+#SBATCH --error=${HOME}/log/${basename}.err    # stderr
 
 cd $scriptpath/../
-if [[ -n "\$SLURM_JOB_ID" ]] || [[ -n "\$PBS_JOBID" ]]; then
-	$command --file "$filename"
-else
-	$command --file "$filename" >  "$logFolder/${basename}.log" 2> "$logFolder/${basename}.err"
-fi
+$command --file "$filename"
 EOF
 }
 
+mkdir -p slurmscripts
 
 function putScriptRedir {
 	command="$1"
 	basename="$2"
-	filename="$3"
-	memory="$4"
-	putScript "$command" "$basename" "$filename" "$memory" > "$scriptFolder/${basename}.sh"
-	chmod u+x "$scriptFolder/${basename}.sh"
+	putScript "$command" "$basename" "$filename" > "slurmscripts/${basename}.sh"
 }
 
 
-for filename in $datasetFolder/*; do
+Pipenv="pipenv run python"
+
+for filename in $HOME/data/*; do
 	basefilename="$(basename $filename)"
-	memory=$kMaxMemory
-	putScriptRedir "uv run src/bidirectional_solver.py" "bidir_$basefilename" "$filename" "$memory"
-	putScriptRedir "uv run src/attractor_solver.py --algo min" "attr_$basefilename" "$filename" "$memory"
-	# putScriptRedir "uv run src/grammar_solver.py" "grammar_$basefilename" "$filename"
-	putScriptRedir "uv run src/slp_solver.py" "slp_$basefilename" "$filename" "$memory"
-	# putScriptRedir "uv run src/slp_naive.py" "slpnaive_$basefilename" "$filename" "100"
-done
+	putScriptRedir "$Pipenv src/bidirectional_solver.py" "bidir_$basefilename" "$filename"
+	putScriptRedir "$Pipenv src/attractor_solver.py --algo min" "attr_$basefilename" "$filename"
+	putScriptRedir "$Pipenv src/grammar_solver.py" "grammar_$basefilename" "$filename"
+	putScriptRedir "$Pipenv src/slp_solver.py" "slp_$basefilename" "$filename"
+done 
