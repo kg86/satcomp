585442605c050b7930bb94000aecf31930d7d02f
Dominik Köppl <git@dkppl.de>
2022-04-20 13:53:35 +0900

    fix docker scripts

--- shell/docker/gen.sh
+++ shell/docker/gen.sh
@@ -5,4 +5,8 @@ tmpfile=$(mktemp)
 tar -cf $tmpfile ../../
 mv $tmpfile solver.tar
 docker build --memory 16G -t satsolver .
-docker run -it --memory=16G sat 
+container_id=$(docker run --memory=16G -d -it --name satsolver satsolver)
+docker exec -it $container_id bash /run.sh
+docker cp $container_id:/home/share/solver/shell/docker/plot.tar .
+docker container stop $container_id 
+docker container rm $container_id 
