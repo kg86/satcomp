04126deb24255a18be96290ccedc104edb1ca8cb
Dominik Köppl <git@dkppl.de>
2022-04-21 08:39:42 +0900

    +measure_datasets.sh

--- shell/measure_datasets.sh
+++ shell/measure_datasets.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/env bash
-
+#!/bin/zsh
 scriptpath=`dirname $(readlink -f "$0")` 
 cd "$scriptpath"
-
 mkdir -p measure/log measure/scripts
 for datafolder in $scriptpath/../data/*; do
 	[[ -d "$datafolder" ]] || continue
@@ -14,8 +12,8 @@ if which sbatch >/dev/null 2>&1; then
 		sbatch $i; 
 	done
 else
-	for script in measure/scripts/*.sh; do 
-		timeout 1h $script > measure/log/$(basename $script .sh).log 2> measure/log/$(basename $script .sh).err
+	for i in measure/scripts/*.sh; do 
+	timeout 1h $script > measure/log/$(basename $script .sh).log 2> measure/log/$(basename $script .sh).err
 	done
 	./concat_json.sh measure/log > measure/benchmark.json
 	cat measure/benchmark.json | jq '[.[] | {algo: .["algo"], file: .["file_name"], outputsize: .["factor_size"] }]' | tee measure/stats.json
