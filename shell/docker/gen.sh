0b96219ae104b67d8d6bb21157d915a394699515
Dominik Köppl <git@dkppl.de>
2022-04-20 12:48:55 +0900

    update scripts

--- shell/docker/gen.sh
+++ shell/docker/gen.sh
@@ -1,16 +1,8 @@
-#!/usr/bin/env bash
-
-scriptpath=`dirname $(readlink -f "$0")` 
-cd "$scriptpath"
-
+#!/bin/bash
 set -x
 set -e
 tmpfile=$(mktemp)
 tar -cf $tmpfile ../../
 mv $tmpfile solver.tar
 docker build --memory 16G -t satsolver .
-container_id=$(docker run --memory=16G -d -it --name satsolver satsolver)
-docker exec -it $container_id bash /run.sh
-docker cp $container_id:/home/share/solver/shell/docker/plot.tar .
-docker container stop $container_id 
-docker container rm $container_id 
+docker run -it --memory=16G sat 
