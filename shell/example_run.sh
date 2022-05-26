94135ee9a3c4a459cdb73ed3463b2fe4fc3535a1
Dominik Köppl <git@dkppl.de>
2022-05-26 14:26:00 +0900

    translate shell scripts to bash

--- shell/example_run.sh
+++ shell/example_run.sh
@@ -9,20 +9,20 @@ if [[ ! -d canterbury-corpus ]]; then
 	git clone https://github.com/pfalcon/canterbury-corpus
 fi
 
-mkdir -p example_run/log example_run/splitted example_run/datasets
-cp canterbury-corpus/canterbury/* example_run/datasets
-cp canterbury-corpus/calgary/* example_run/datasets
-cp canterbury-corpus/artificial/aaa.txt example_run/datasets
-cp canterbury-corpus/artificial/random.txt example_run/datasets
-cp canterbury-corpus/large/* example_run/datasets
-rm example_run/datasets/SHA1SUM
+mkdir -p "$scriptpath"/example_run/log "$scriptpath"/example_run/splitted "$scriptpath"/example_run/datasets
+cp canterbury-corpus/canterbury/* "$scriptpath"/example_run/datasets
+cp canterbury-corpus/calgary/* "$scriptpath"/example_run/datasets
+cp canterbury-corpus/artificial/aaa.txt "$scriptpath"/example_run/datasets
+cp canterbury-corpus/artificial/random.txt "$scriptpath"/example_run/datasets
+cp canterbury-corpus/large/* "$scriptpath"/example_run/datasets
+rm "$scriptpath"/example_run/datasets/SHA1SUM
 
 for dataset in example_run/datasets/*; do 
-	[[ -f "$dataset" ]] && [[ -r "$dataset" ]] && ./splitter.sh $dataset example_run/splitted
+	[[ -f "$dataset" ]] && [[ -r "$dataset" ]] && "$scriptpath/splitter.sh" $dataset example_run/splitted
 done
 
-./generate_slurmscripts.sh example_run/splitted example_run/log example_run/scripts
-for script in example_run/scripts/*.sh; do
-	timeout 1h $script > example_run/log/$(basename $script .sh).log 2> example_run/log/$(basename $script .sh).err
+"$scriptpath/generate_slurmscripts.sh" "$scriptpath/example_run/splitted" "$scriptpath/example_run/log" "$scriptpath/example_run/scripts"
+for script in "$scriptpath/example_run/scripts/"*.sh; do
+	timeout 1h $script > "$scriptpath/example_run/log/"$(basename $script .sh).log 2> "$scriptpath/example_run/log/"$(basename $script .sh).err
 done
-./concat_json.sh example_run/log > example_run/benchmark.json
+"$scriptpath/concat_json.sh" "$scriptpath/example_run/log" > "$scriptpath/example_run/benchmark.json"
--- shell/measure_datasets.sh
+++ shell/measure_datasets.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 scriptpath=`dirname $(readlink -f "$0")` 
 cd "$scriptpath"
