ace35c53c2e013287276a56223c73e6a235c2dd6
Dominik Köppl <git@dkppl.de>
2025-07-31 08:36:56 +0900

    script: fix wholefolder variable

--- benchmark.sh
+++ benchmark.sh
@@ -14,6 +14,7 @@ if [ -z "$2" ]; then
 	die "Usage: $0 <dataset_dir> <log_dir>"
 fi
 datasetFolder="$(readlink -f "$1")"
+wholeFolder="$datasetFolder/whole"
 logFolder="$(readlink -f "$2")"
 mkdir -p "$logFolder" || die "Failed to create directory $logFolder"
 
@@ -28,16 +29,15 @@ if [ ! -d "$datasetFolder/whole" ]; then
 		git clone https://github.com/pfalcon/canterbury-corpus || die "Failed to clone canterbury-corpus"
 	fi
 	mkdir whole || die "Failed to create directory whole"
-	datasetFolder="$datasetFolder/whole"
 	cd canterbury-corpus
-	cp canterbury/* $datasetFolder/ || die "Failed to copy canterbury-corpus files to $datasetFolder directory"
-	cp calgary/* $datasetFolder/ || die "Failed to copy calgary-corpus files to $datasetFolder directory"
-	cp large/* $datasetFolder/ || die "Failed to copy large-corpus files to $datasetFolder directory"
-	cp artificial/* $datasetFolder/ || die "Failed to copy artificial-corpus files to $datasetFolder directory"
-	rm $datasetFolder/SHA1SUM
+	cp canterbury/* $wholeFolder/ || die "Failed to copy canterbury-corpus files to $wholeFolder directory"
+	cp calgary/* $wholeFolder/ || die "Failed to copy calgary-corpus files to $wholeFolder directory"
+	cp large/* $wholeFolder/ || die "Failed to copy large-corpus files to $wholeFolder directory"
+	cp artificial/* $wholeFolder/ || die "Failed to copy artificial-corpus files to $wholeFolder directory"
+	rm $wholeFolder/SHA1SUM
 fi
 
 cd "$scriptpath" || die "Failed to change directory to $scriptpath"
-for file in "$datasetFolder/whole"/*; do 
+for file in "$wholeFolder"/*; do 
 	 pipenv run python3 tests/benchmark.py --file "$file" --outdir "$logFolder" --maxtime 3600 --maxmem 64000000000; 
  done
